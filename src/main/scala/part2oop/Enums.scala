a852685fd5d60d441bf85d366e58dd412b9ef312
login <login@pop-os.localdomain>
2023-06-12 20:28:34 -0400

    Enums

---
+++ src/main/scala/part2oop/Enums.scala
@@ -0,0 +1,48 @@
+package part2oop
+
+object Enums {
+
+  enum Permissions {
+    case READ, WRITE, EXECUTE, NONE
+
+    // add fields/methods
+    def openDocument(): Unit =
+      if (this == READ) println("opening document...")
+      else println("reading not allowed.")
+  }
+
+  val somePermissions: Permissions = Permissions.READ
+
+
+  // construct args
+  enum PermissionsWithBits(bits: Int) {
+    case READ extends PermissionsWithBits(4) // 100
+    case WRITE extends PermissionsWithBits(2) // 010
+    case EXECUTE extends PermissionsWithBits(1) // 001
+    case NONE extends PermissionsWithBits(0) // 000
+  }
+
+  object PermissionsWithBits {
+    def fromBits(bits: Int): PermissionsWithBits = //watever
+      PermissionsWithBits.NONE
+  }
+
+  // standard API
+  val somePermissionsOridnal = somePermissions.ordinal
+  val allPermissions = PermissionsWithBits.values // array of all possible values of the enum
+  val readPermission: Permissions = Permissions.valueOf("READ") // Permissions.READ
+
+  /*
+    java way
+    enum Thing {
+      case A(2), B(3)
+    }
+   */
+
+  def main(args: Array[String]): Unit = {
+    somePermissions.openDocument()
+    println(somePermissionsOridnal)
+  }
+
+}
+
